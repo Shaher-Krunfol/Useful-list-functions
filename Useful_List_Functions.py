e6d6f3008f073cf756a77ab9b195ae76cb1d6167
Shaher Krunfol <147998451+Shaher-Krunfol@users.noreply.github.com>
2025-07-22 17:19:47 +0300

    Add files via upload

---
+++ Useful_List_Functions.py
@@ -0,0 +1,96 @@
+def largest_number(list):
+    return max(list)
+
+def smallest_number(list):
+    return min(list)
+
+def average_number(list):
+    return sum(list) / len(list)
+
+def median_number(list):
+    sorted_list = sorted(list)
+    n = len(sorted_list)
+    mid = n // 2
+    if n % 2 == 0:
+        return (sorted_list[mid - 1] + sorted_list[mid]) / 2
+    else:
+        return sorted_list[mid]
+    
+def mode_number(list):
+    for num in list:
+        if list.count(num) > 1:
+            return num
+        
+def remove_duplicates(list):
+    return list(set(list))
+        
+def add_to_list(list, value):
+    list.append(value)
+    return list
+
+def remove_from_list(list, value):
+    if value in list:
+        list.remove(value)
+    return list
+
+def sort_list(list):
+    return sorted(list)
+
+def reverse_list(list):
+    return list[::-1]
+
+def find_index(list, value):
+    if value in list:
+        return list.index(value)
+    else:
+        return -1
+    
+def count_occurrences(list, value):
+    return list.count(value)
+
+def clear_list(list):
+    list.clear()
+    return list
+
+def copy_list(list):
+    return list.copy()
+
+def concatenate_lists(list1, list2):
+    return list1 + list2
+
+def intersection(list1, list2):
+    return list(set(list1) & set(list2))
+
+def union(list1, list2):
+    return list(set(list1) | set(list2))
+
+def difference(list1, list2):
+    return list(set(list1) - set(list2))
+
+def is_subset(list1, list2):
+    return set(list1).issubset(set(list2))
+
+def is_superset(list1, list2):
+    return set(list1).issuperset(set(list2))
+
+def zip_lists(list1, list2):
+    return list(zip(list1, list2))
+
+def unzip_list(zipped_list):
+    return list(zip(*zipped_list))
+
+def flatten_list(nested_list):
+    flat_list = []
+    for sublist in nested_list:
+        if isinstance(sublist, list):
+            flat_list.extend(flatten_list(sublist))
+        else:
+            flat_list.append(sublist)
+    return flat_list
+
+def chunk_list(list, chunk_size):
+    return [list[i:i + chunk_size] for i in range(0, len(list), chunk_size)]
+
+def rotate_list(list, n):
+    n = n % len(list)  # Handle cases where n is larger than the list length
+    return list[-n:] + list[:-n]
